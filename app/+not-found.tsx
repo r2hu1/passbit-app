6feaed0dc0ca93c84150d5d9af1efd257b166837
r2hu1 <hi@rahul.eu.org>
2025-06-10 15:59:42 +0530

    layouts

---
+++ app/+not-found.tsx
@@ -0,0 +1,14 @@
+import { Link, Stack } from "expo-router";
+import { View } from "react-native";
+import { Text } from "~/components/ui/text";
+
+export default function NotFoundScreen() {
+  return (
+    <View>
+      <Text>This screen doesn't exist.</Text>
+      <Link href="/">
+        <Text>Go to home screen!</Text>
+      </Link>
+    </View>
+  );
+}
---
+++ app/_layout.tsx
@@ -0,0 +1,94 @@
+import "~/global.css";
+
+import {
+  DarkTheme,
+  DefaultTheme,
+  Theme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { Appearance, Platform, View } from "react-native";
+import { NAV_THEME } from "~/lib/constants";
+import { useColorScheme } from "~/lib/useColorScheme";
+import { PortalHost } from "@rn-primitives/portal";
+import { setAndroidNavigationBar } from "~/lib/android-navigation-bar";
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from "react-native-safe-area-context";
+
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export { ErrorBoundary } from "expo-router";
+
+const usePlatformSpecificSetup = Platform.select({
+  web: useSetWebBackgroundClassName,
+  android: useSetAndroidNavigationBar,
+  default: noop,
+});
+
+export default function RootLayout() {
+  usePlatformSpecificSetup();
+  const { isDarkColorScheme } = useColorScheme();
+
+  return (
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+        <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+        <Stack>
+          <Stack.Screen
+            name="welcome"
+            options={{
+              headerShown: false,
+              animation: "default",
+            }}
+          />
+          <Stack.Screen
+            name="login"
+            options={{
+              headerShown: false,
+              animation: "default",
+            }}
+          />
+          <Stack.Screen
+            name="register"
+            options={{
+              headerShown: false,
+              animation: "default",
+            }}
+          />
+        </Stack>
+        <PortalHost />
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
+}
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === "web" && typeof window === "undefined"
+    ? React.useEffect
+    : React.useLayoutEffect;
+
+function useSetWebBackgroundClassName() {
+  useIsomorphicLayoutEffect(() => {
+    // Adds the background color to the html element to prevent white background on overscroll.
+    document.documentElement.classList.add("bg-background");
+  }, []);
+}
+
+function useSetAndroidNavigationBar() {
+  React.useLayoutEffect(() => {
+    setAndroidNavigationBar(Appearance.getColorScheme() ?? "light");
+  }, []);
+}
+
+function noop() {}
